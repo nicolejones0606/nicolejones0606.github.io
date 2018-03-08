4064628fd1c0db9b51ebadc368fbada693cf9195
Jones <nmj24@om106t.oldmain.txstate.edu>
2018-03-07 20:33:31 -0600

    interaction project final

--- js/javascript.js
+++ js/javascript.js
@@ -1,110 +1,85 @@
-// Empty array to hold the profile items
-var profileElements = [];
+// Setup variables:
+var donate;
+var netWorth;
+var twitter;
 
-// Set up the form interaction so that everytime the form changes, the function updateProfile will run.
-var profileForm = document.getElementById('profileForm');
-profileForm.addEventListener('change', updateProfile, false);
+// Create the function to process the form. This function will run when the form is submitted. You need to create this function before attaching it to the event, below
 
-/***
-This is code that's going to run when the form changes. We're not quite to the point where we can write it yet, but you can probably get a good sense of what it does.
-***/
-function updateProfile(e) {
+var processForm = function(event) {
 
-  var metrics = document.getElementsByName("metrics[]");
+	// Radio Buttons
+	var radioButtons = document.getElementsByName('donate');
+	for (var i = 0; i < radioButtons.length; i++) {
+			if (radioButtons[i].checked) {
+				donate = radioButtons[i].value;
+				break; // quits the for loop once you find the option that's selected
+			}
+	}
+	console.log("Do they donate: " + donate);
 
-  //Remove everything from backpack.
-  profileElements = [];
 
-  // Loop through the items, and if they're checked, add them to the backpack array.
-  for (var i = 0; i < items.length; i++) {
-    if (metrics[i].selected == "1 million") {
-      profileElements.push(metrics[i].value)
-    }
-  }
+	// Select for Net Worth
+	var netWorthElement = document.getElementById('netWorth');
+	var netWorthValue = netWorthElement.options[netWorthElement.selectedIndex].value;
+	console.log("Net Worth: " + netWorthValue);
 
-  console.log(profileElements);
+  // Select for Twitter
+  var twitterElement = document.getElementById('twitter');
+  var twitterValue = twitterElement.options[twitterElement.selectedIndex].value;
+  console.log("Twitter: " + twitterValue);
 
-  printItems();
-  checkChallenges();
-
+//what to display in the response window
+if (donate == "yesDonate" && netWorthValue == "50bil" && twitterValue == "45mil") {
+	var target = document.querySelector (".content .dynamic");
+	var newHTML = '<h3>Bill Gates</h3> <a class="twitter-timeline" href="https://twitter.com/BillGates?ref_src=twsrc%5Etfw">Tweets by BillGates</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+	target.innerHTML = newHTML;
+} else if (donate == "yesDonate" && netWorthValue == "1bil" && twitterValue == "42mil") {
+		var target = document.querySelector (".content .dynamic");
+		var newHTML = '<h3>Oprah Winfrey</h3> <a class="twitter-timeline" href="https://twitter.com/Oprah?ref_src=twsrc%5Etfw">Tweets by Oprah</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+		target.innerHTML = newHTML;
+} else if (donate == "noDonate" && netWorthValue == "1bil" && twitterValue == "49mil") {
+		var target = document.querySelector (".content .dynamic");
+		var newHTML = '<h3>Donald Trump</h3> <a class="twitter-timeline" href="https://twitter.com/realDonaldTrump?ref_src=twsrc%5Etfw">Tweets by realDonaldTrump</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+		target.innerHTML = newHTML;
+} else if (donate == "noDonate" && netWorthValue == "100mil" && twitterValue == "60mil") {
+	var target = document.querySelector (".content .dynamic");
+	var newHTML = '<h3>Kim Kardashian</h3> <a class="twitter-timeline" href="https://twitter.com/KimKardashian?ref_src=twsrc%5Etfw">Tweets by KimKardashian</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+	target.innerHTML = newHTML;
+} else {
+	var target = document.querySelector (".content .dynamic");
+	var newHTML = '<h3>No such celebrity exists!</h3>';
+	target.innerHTML = newHTML;
 }
 
+	event.preventDefault(); // Stop the form from submitting.
 
-/***
+} // end processForm
 
-To practice working with arrays, let's display a list of the items in the backpack array. Something like "Cheese, Key, Flour."
+// Find the form:
+var form = document.getElementById('profileForm');
 
-Bonus: include commas, but not before the first item.
-
-The following code will run each time the form changes.
-
-***/
-
-function printItems() {
-
-  var metricsMessage = ""
-
-  // Your code here.
+// Attach a function to the form's submit event:
+form.addEventListener('submit', processForm, false);
 
 
 
-  /*** This just updates the page. ***/
-  var metricsMessageElement = document.getElementById('itemsMessage');
-  var newItemsMessage = document.createTextNode(itemsMessage);
-  itemsMessageElement.innerHTML = "";
-  itemsMessageElement.appendChild(newItemsMessage);
-
-} //printItems
+// fun javascript for changing the visual appearance as the user moves through the form
+var charityForm = document.querySelector('.charityForm');
+var netWorthForm = document.querySelector('.netWorthForm');
+var twitterForm = document.querySelector('.twitterForm');
 
 
+var formClick = function(event) {
+	document.querySelector('.charityForm').style.background = 'white';
+}
+charityForm.addEventListener('click', formClick, false);
 
+var formClick1 = function(event) {
+document.querySelector('.netWorthForm').style.background = 'white';
+}
+netWorthForm.addEventListener('click', formClick1, false);
 
-/**** Now we're going to create some arrays describing the requirements to pass some challenges. ****/
-
-var cakeRequirements = ["1mil","",""]
-
-
-
-
-// This code will run everytime the form changes.
-function checkChallenges() {
-
-  var challengeMessage = "";
-
-  /***
-  We need to decide whether the person can complete the challenge based on what's in their backpack. Write pseudocode that would check whether someone has the items needed to pass the cake challenge.
-  ***/
-
-
-
-
-
-  /** scrolling ... ***/
-
-
-
-
-
-
-
-
-
-  /** Here's one way to do it:
-
-  Create a variable called requirementsFilled
-  Loop through each requirement
-    Loop through each backpack item
-      If that item == the current requirement, increase requirementsFilled by 1
-  Check if requirementsFilled = the length of the requirements array
-  **/
-
-
-
-  /*** This just updates the page. ***/
-  var challengeMessageElement = document.getElementById('challengeMessage');
-  var newChallengeMessage = document.createTextNode(challengeMessage);
-  challengeMessageElement.innerHTML = "";
-  challengeMessageElement.appendChild(newChallengeMessage);
-
-
-} //checkChallenges
+var formClick2 = function(event) {
+document.querySelector('.twitterForm').style.background = 'white';
+}
+twitterForm.addEventListener('click', formClick2, false);
